2a5aedc1ec3ce44942f9978c936967761a63fea9
Leonardo1952 <lo88980@gmail.com>
2020-08-12 11:43:59 -0300

    Finalizacao do banco

--- lib/helpers/contact_helper.dart
+++ lib/helpers/contact_helper.dart
@@ -59,8 +59,46 @@ class ContactHelper {
     }
 
   }
+  //Para exclusão do contato
+  Future<int>deleteContact(int id) async{
+    Database dbContact = await db;
+    return await dbContact.delete(contactTable, where: "$idColumn = ?",
+        whereArgs: [id]);
+  }
+
+  //Atualizar contatos
+  Future<int>updateContact(Contact contact) async{
+    Database dbContact = await db;
+    dbContact.update(contactTable, contact.toMap(),
+        where: "$idColumn = ?",
+        whereArgs: [contact.id]);
+  }
+
+  //Obtenção de todos os contatos
+  Future<List>getAllContacts() async{
+    Database dbContact = await db;
+    List listMap = await dbContact.rawQuery("SELECT = FROM $contactTable");
+    List<Contact> listContact = List();
+    for(Map m in listMap){
+      listContact.add(Contact.fromMap(m));
+    }
+    return listContact;
+  }
+  
+  //Obter numero de contatos da lista
+  Future<int>getNumber() async{
+    Database dbContact = await db;
+    return Sqflite.firstIntValue(await dbContact.rawQuery("SELECT COUNT(*) FROM $contactTable"));
+  }
+
+  //Fechar Banco
+  Future close() async{
+    Database dbContact = await db;
+    dbContact.close();
+  }
 }
 // id name  email   phone   img
+//Molde para contatos
 
 class Contact {
 
