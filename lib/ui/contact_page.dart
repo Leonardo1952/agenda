82555e2bbb7687b51aef1b79a4258dd42a56afc7
Leonardo1952 <lo88980@gmail.com>
2020-08-19 12:26:09 -0300

    criacao tela de contato

---
+++ lib/ui/contact_page.dart
@@ -0,0 +1,47 @@
+import 'package:agenda/helpers/contact_helper.dart';
+import 'package:flutter/material.dart';
+
+class ContactPage extends StatefulWidget {
+
+  final Contact contact;
+
+  ContactPage({this.contact}); //Chaves para deixar parametro como opcional
+
+  @override
+  _ContactPageState createState() => _ContactPageState();
+}
+
+class _ContactPageState extends State<ContactPage> {
+
+    Contact _editedContact;
+
+  @override
+  void initState() {
+    super.initState();
+
+    if(widget.contact == null){
+      _editedContact = Contact();
+    } else {
+      _editedContact = Contact.fromMap(widget.contact.toMap());
+    }
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: Text(_editedContact.name ?? "Novo Contato"),
+        centerTitle: true,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        child: Icon(Icons.save),
+        backgroundColor: Colors.red,
+      ),
+    );
+  }
+
+
+}
