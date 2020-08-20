0e658d60db77a5729088d91ba90b5a2af97e67f4
Leonardo1952 <lo88980@gmail.com>
2020-08-20 12:36:17 -0300

    ordenacao dos contatos

--- lib/ui/contact_page.dart
+++ lib/ui/contact_page.dart
@@ -1,5 +1,5 @@
 import 'dart:io';
-
+import 'package:image_picker/image_picker.dart';
 import 'package:agenda/helpers/contact_helper.dart';
 import 'package:flutter/material.dart';
 
@@ -72,6 +72,14 @@ class _ContactPageState extends State<ContactPage> {
                                 ? FileImage(File(_editedContact.img))
                                 : AssetImage("images/person.png"))),
                   ),
+                  onTap: () {
+                    ImagePicker.pickImage(source: ImageSource.camera).then((file){
+                      if(file == null) return;
+                      setState(() {
+                        _editedContact.img = file.path;
+                      });
+                    });
+                  },
                 ),
                 TextField(
                   controller: _nameController,
--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,11 +1,12 @@
 import 'dart:io';
 
-
 import 'package:path/path.dart';
 import 'package:agenda/helpers/contact_helper.dart';
 import 'package:flutter/material.dart';
-
+import 'package:url_launcher/url_launcher.dart';
 import 'contact_page.dart';
+
+enum OrderOptions {orderaz, orderza}
 
 class HomePage extends StatefulWidget {
   @override
@@ -30,6 +31,21 @@ class _HomePageState extends State<HomePage> {
         title: Text("Contatos"),
         backgroundColor: Colors.red,
         centerTitle: true,
+        actions: <Widget>[
+          PopupMenuButton<OrderOptions>(
+            itemBuilder: (context) => <PopupMenuEntry<OrderOptions>>[
+              const PopupMenuItem<OrderOptions>(
+                child: Text("Ordenar de A-Z"),
+                value: OrderOptions.orderaz,
+              ),
+              const PopupMenuItem<OrderOptions>(
+                child: Text("Ordenar de Z-A"),
+                value: OrderOptions.orderza,
+              )
+            ],
+            onSelected: _orderList,
+          )
+        ],
       ),
       backgroundColor: Colors.white,
       floatingActionButton: FloatingActionButton(
@@ -90,8 +106,67 @@ class _HomePageState extends State<HomePage> {
         ),
       ),
       onTap: () {
-        _showContactPage(context, contact: contacts[index]);
+        _showOptions(context, index);
       },
+    );
+  }
+  void _showOptions(BuildContext context, int index){
+    showModalBottomSheet(
+      context: context,
+      builder: (context){
+        return BottomSheet(
+          onClosing: (){},
+          builder: (context){
+            return Container(
+              padding: EdgeInsets.all(10.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: FlatButton(
+                      child: Text("Ligar",
+                      style: TextStyle(color: Colors.red, fontSize: 20.0),
+                    ),
+                    onPressed: (){
+                        launch("tel:${contacts[index].phone}");
+                        Navigator.pop(context);
+                    },
+                  )
+                  ),
+                  Padding(
+                      padding: EdgeInsets.all(10.0),
+                      child: FlatButton(
+                        child: Text("Editar",
+                          style: TextStyle(color: Colors.red, fontSize: 20.0),
+                        ),
+                        onPressed: (){
+                          Navigator.pop(context);
+                            _showContactPage(context, contact: contacts[index]);
+                        },
+                      )
+                  ),
+                  Padding(
+                      padding: EdgeInsets.all(10.0),
+                      child: FlatButton(
+                        child: Text("Excluir",
+                          style: TextStyle(color: Colors.red, fontSize: 20.0),
+                        ),
+                        onPressed: (){
+                            helper.deleteContact(contacts[index].id);
+                            setState(() {
+                              contacts.removeAt(index);
+                              Navigator.pop(context);
+                            });
+                        },
+                      )
+                  ),
+                ],
+              )
+            );
+          },
+        );
+    }
     );
   }
 
@@ -114,6 +189,24 @@ class _HomePageState extends State<HomePage> {
       setState(() {
         contacts = list;
       });
+    });
+  }
+
+  void _orderList(OrderOptions result) {
+    switch(result){
+      case OrderOptions.orderaz:
+        contacts.sort((a,b) {
+          return a.name.toLowerCase().compareTo(b.name.toLowerCase());
+        });
+        break;
+      case OrderOptions.orderza:
+        contacts.sort((a,b) {
+          return b.name.toLowerCase().compareTo(a.name.toLowerCase());
+        });
+        break;
+    }
+    setState(() {
+
     });
   }
 }
